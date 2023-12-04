58f53c2a63f31bb54cbe8e91ea4b38431a437b49
Dav1dManzanares <140092494+Dav1dManzanares@users.noreply.github.com>
2023-12-04 15:25:02 +0000

    cambios 1

--- Card war v1/Lobby/locate.js
+++ Card war v1/Lobby/locate.js
@@ -1,3 +1,20 @@
 function redirection() {
     location.href = "../Game/index.html"
 }
+
+function mostrarOcultarReglas() {
+    const reglas = document.getElementById("reglas");
+    const boton = document.getElementById("botonReglas");
+  
+    if (reglas.style.display === "none") {
+      reglas.style.display = "block"; 
+      boton.textContent = "Hide Rules"; 
+    } else {
+      reglas.style.display = "none"; 
+      boton.textContent = "Show Rules"; 
+    }
+  }
+  
+ 
+  document.getElementById("botonReglas").addEventListener("click", mostrarOcultarReglas);
+  
